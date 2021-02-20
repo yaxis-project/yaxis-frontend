4e302640901596eccebba136bb8fb56fbd0c3cca
getgouda <getgouda@yaxis.io>
2021-02-20 17:15:54 -0500

    Extra responsiveness to Top Nav accounts buttons

--- src/components/TopBar/components/AccountButton.tsx
+++ src/components/TopBar/components/AccountButton.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { useWallet } from 'use-wallet'
 import useModal from '../../../hooks/useModal'
 import useTVL from '../../../hooks/useComputeTVL'
-import { Button, Menu, Dropdown } from 'antd'
+import { Button, Menu, Dropdown, Row, Col } from 'antd'
 import { CaretDownOutlined } from '@ant-design/icons'
 
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon' // @ts-ignore
@@ -28,66 +28,85 @@ const AccountButton: React.FC<AccountButtonProps> = (props) => {
 
 	return (
 		<StyledAccountButton>
-			<StyledSpan>
+			<StyledCol>
 				<span>YAX</span>
 				<span>${new BigNumber(yaxisPrice).toFixed(2)}</span>
-			</StyledSpan>
-			{!account ? (
-				<Button
-					onClick={handleUnlockClick}
-					style={{
-						fontWeight: 'bold',
-						fontSize: '18px',
-						height: '38px',
-					}}
-				>
-					Connect
-				</Button>
-			) : (
-					<Fragment>
-						<Jazzicon
-							diameter={36}
-							seed={jsNumberForAddress(account)}
-						/>
-						<Dropdown
-							placement="bottomRight"
-							overlay={
-								<Menu>
-									<Menu.Item onClick={() => reset()}>
-										Logout
+			</StyledCol>
+			<Col>
+				{!account ? (
+					<StyledButton
+						onClick={handleUnlockClick}
+					>
+						Connect
+					</StyledButton>
+				) : (
+						<Fragment>
+							<Jazzicon
+								diameter={36}
+								seed={jsNumberForAddress(account)}
+							/>
+							<Dropdown
+								placement="bottomRight"
+								overlay={
+									<Menu>
+										<Menu.Item onClick={() => reset()}>
+											Logout
 								</Menu.Item>
-								</Menu>
-							}
-						>
-							<CaretDownOutlined />
-						</Dropdown>
-					</Fragment>
-				)}
+									</Menu>
+								}
+							>
+								<CaretDownOutlined />
+							</Dropdown>
+						</Fragment>
+					)}
+			</Col>
 		</StyledAccountButton>
 	)
 }
 
-const StyledSpan = styled.span`
+const StyledCol = styled(Col)`
 	color: white;
 	padding: 8px 12px;
 	border: 1px solid white;
-	font-size: 18px;
+	font-size: 1em;
 	line-height: 1em;
 	border-radius: 12px;
 	font-weight: 700;
 	margin-right: 18px;
 	> span:first-child {
 		margin-right: 15px;
+	}
+
+	@media only screen and (max-width: 1100px) {
+		> span:first-child {
+			margin-right: 5px;
+		}
+	}
+`
+
+const StyledButton = styled(Button)`
+	font-weight: bold;
+	font-size: 1em;
+	height: 38px;
+
+	@media only screen and (max-width: 1100px) {
+		height: 32px;
+	}
 }
 `
 
-const StyledAccountButton = styled.div`
+const StyledAccountButton = styled(Row)`
 	display: flex;
 	align-items: center;
+	font-size: 1rem;
 
 	.ant-dropdown-trigger {
 		margin-left: 6px;
 		color: white;
+	}
+
+	@media only screen and (max-width: 1100px) {
+		font-size: 0.8rem;
 	}
 `
 
