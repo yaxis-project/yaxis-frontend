6bc334d18d6e26d352eaac83c123c5b15527120e
getgouda <getgouda@yaxis.io>
2021-03-20 02:47:01 -0400

    Refactor useApproveStaking hook to handle its own error & loading state. Checks balance against estimated gas

--- src/hooks/useApproveStaking.ts
+++ src/hooks/useApproveStaking.ts
@@ -1,28 +1,56 @@
-import { useCallback } from 'react'
-
-import useYaxis from './useYaxis'
+import { useState, useEffect, useCallback, useMemo } from 'react'
+import { notification } from 'antd'
+import useGlobal from './useGlobal'
 import { useWeb3React } from '@web3-react/core'
 import {
 	approve,
 	getXSushiStakingContract,
 	getYaxisContract,
 } from '../yaxis/utils'
+import { ethers } from 'ethers'
+import BN from 'bignumber.js'
 
 const useApproveStaking = () => {
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
+
 	const { account } = useWeb3React()
-	const yaxis = useYaxis()
-	const lpContract = getYaxisContract(yaxis)
-	const contract = getXSushiStakingContract(yaxis)
+	const { yaxis, balance } = useGlobal()
+	const lpContract = useMemo(() => getYaxisContract(yaxis), [yaxis])
+	const contract = useMemo(() => getXSushiStakingContract(yaxis), [yaxis])
+
+	useEffect(() => {
+		const canAfford = async () => {
+			const gas = await lpContract?.methods
+				.approve(contract.options.address, ethers.constants.MaxUint256)
+				.estimateGas({ from: account })
+			if (new BN(gas).gt(balance))
+				setError('Your Ethereum balance is too low')
+		}
+		canAfford()
+	}, [account, lpContract, contract, balance])
 
 	const handleApprove = useCallback(async () => {
 		try {
-			return await approve(lpContract, contract, account)
+			setLoading(true)
+			notification.info({
+				message: 'Please approve YAX staking amount.',
+			})
+
+			const tx = await approve(lpContract, contract, account)
+			setLoading(false)
+			return tx
 		} catch (e) {
+			setError(e)
+			notification.error({
+				description: e.message,
+				message: `Unable to approve YAX for staking:`,
+			})
 			return false
 		}
 	}, [account, lpContract, contract])
 
-	return { onApprove: handleApprove }
+	return { onApprove: handleApprove, loading, error }
 }
 
 export default useApproveStaking
