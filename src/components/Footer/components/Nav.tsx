26335864254f7dcb88d35b491bdbd0210fe7c114
getgouda <getgouda@yaxis.io>
2021-03-23 18:04:25 -0400

    Change Footer blog logo to a book

--- src/components/Footer/components/Nav.tsx
+++ src/components/Footer/components/Nav.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Row, Col } from 'antd'
-import blog from '../../../assets/img/icons/ghost.svg'
-import discord from '../../../assets/img/icons/discord.svg'
-import github from '../../../assets/img/icons/github.svg'
-import twitter from '../../../assets/img/icons/twitter.svg'
+import { ReactComponent as discord } from '../../../assets/img/icons/discord.svg'
+import { ReactComponent as github } from '../../../assets/img/icons/github.svg'
+import { ReactComponent as twitter } from '../../../assets/img/icons/twitter.svg'
 import quantstamp from '../../../assets/img/icons/quantstamp.svg'
-
-// const menu = (
-// 	<Menu>
-// 		<Menu.Item>
-// 			<a
-// 				target="_blank"
-// 				rel="noopener noreferrer"
-// 				href="https://etherscan.io/address/0xc330e7e73717cd13fb6ba068ee871584cf8a194f#code"
-// 			>
-// 				yAxis Chef
-// 			</a>
-// 		</Menu.Item>
-// 	</Menu>
-// )
+import Icon, { ReadOutlined } from '@ant-design/icons'
 
 const Nav: React.FC = () => {
 	return (
@@ -42,26 +28,26 @@ const Nav: React.FC = () => {
 			</StyledCol2>
 
 			<StyledCol3 xs={24} sm={24} md={8}>
-				<StyledLink target="_blank" href="https://yaxis.ghost.io">
-					<img src={blog} height="20" alt="Blog Link" />
+				<StyledLink target="_blank" href="https://yaxis.ghost.io" >
+					<ReadOutlined style={{ fontSize: "25px" }} />
 				</StyledLink>
 				<StyledLink
 					target="_blank"
 					href="https://discord.gg/u8KZPCMJ6x"
 				>
-					<img src={discord} height="20" alt="Discord Link" />
+					<HoverableIcon component={discord} />
 				</StyledLink>
 				<StyledLink
 					target="_blank"
 					href="https://github.com/yaxis-project"
 				>
-					<img src={github} height="20" alt="Github Link" />
+					<HoverableIcon component={github} />
 				</StyledLink>
 				<StyledLink
 					target="_blank"
 					href="https://twitter.com/yaxis_project"
 				>
-					<img src={twitter} height="20" alt="Twitter Link" />
+					<HoverableIcon component={twitter} />
 				</StyledLink>
 			</StyledCol3>
 		</StyledRow>
@@ -94,14 +80,9 @@ const StyledCol3 = styled(StyledCol)`
 	}
 `
 
-// const StyledNavLink = styled(NavLink)`
-// 	display: inline-block;
-// 	color: ${(props) => props.theme.color.primary.main};
-// 	padding-left: ${(props) => props.theme.spacing[3]}px;
-// 	padding-right: ${(props) => props.theme.spacing[3]}px;
-// 	text-decoration: none;
-// 	font-size: 18px;
-// `
+const HoverableIcon = styled(Icon)`
+	font-size: 25px;
+`
 
 const StyledLink = styled.a`
 	display: inline-block;
