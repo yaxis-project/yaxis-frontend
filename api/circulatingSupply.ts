759d3842cf22be82b636905771ff9bf4722b883f
getgouda <getgouda@yaxis.io>
2021-04-02 22:28:37 -0400

    House cleaning - api

--- api/circulatingSupply.ts
+++
@@ -1,6 +0,0 @@
-import { supply } from './supply'
-
-export default async (req: any, res: any) => {
-	let supplyResult = await supply()
-	res.status(200).send(supplyResult.circulatingSupply)
-}
--- api/date.ts
+++
@@ -1,4 +0,0 @@
-export default (req: any, res: any) => {
-	const date = new Date().toString()
-	res.status(200).send({ date })
-}
