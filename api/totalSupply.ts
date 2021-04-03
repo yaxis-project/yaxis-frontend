8b28557e014fa22e926d2608e9ecf73e02d18223
getgouda <getgouda@yaxis.io>
2021-04-02 22:28:37 -0400

    House cleaning - api

--- api/totalSupply.ts
+++
@@ -1,7 +0,0 @@
-import { supply } from './supply'
-
-export default async (req: any, res: any) => {
-	res.setHeader('Cache-Control', 's-maxage=180')
-	let supplyResult = await supply()
-	res.status(200).send(supplyResult.totalSupply)
-}
