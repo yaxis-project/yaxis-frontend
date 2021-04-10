49831e9e45c61a26f36f0421e720b147b54907c7
yakattack139 <yakattack139@protonmail.com>
2021-04-10 16:08:33 -0400

    Fix: made card row component more consistent

--- src/views/Staking/components/SavingsOverviewCard.tsx
+++ src/views/Staking/components/SavingsOverviewCard.tsx
@@ -5,10 +5,8 @@ import Value from '../../../components/Value'
 import useYaxisStaking from '../../../hooks/useYAXISStaking'
 import useStakingAPY from '../../../hooks/useStakingAPY'
 import phrases from './translations'
-import {
-	DetailOverviewCard,
-	DetailOverviewCardRow,
-} from '../../../components/DetailOverviewCard'
+import { DetailOverviewCard } from '../../../components/DetailOverviewCard'
+import { CardRow } from '../../../components/ExpandableSidePanel'
 
 import info from '../../../assets/img/info.svg'
 
@@ -29,40 +27,40 @@ export default function SavingsOverviewCard() {
 
 	return (
 		<DetailOverviewCard title={t('Account Overview')}>
-			{/* <DetailOverviewCardRow>
-				<Text>Returns</Text>
-				<Value
-					// numberPrefix="$"
-					value={"TBD"}
-					// extra={`${yaxReturns} YAX`}
-					decimals={2}
-				/>
-			</DetailOverviewCardRow> */}
-			<DetailOverviewCardRow>
-				<Text>YAXIS Staked</Text>
-				<Value value={stakedBalance.toFixed(3)} numberSuffix=" YAXIS" />
-			</DetailOverviewCardRow>
-			<DetailOverviewCardRow>
-				<Tooltip
-					title={
-						<>
-							<Row>YAXIS APY:</Row>
-							<Row>{yaxAPY?.toFixed(2)}%</Row>
-							<Row>CRV APY (20%):</Row>
-							<Row>{metavaultAPY?.toFixed(2)}%</Row>
-						</>
-					}
-				>
-					<Text>Total APY </Text>
-					<img
-						style={{ position: 'relative', top: -1 }}
-						src={info}
-						height="15"
-						alt="YAXIS Supply Rewards"
+			<CardRow
+				main={t('YAXIS Staked')}
+				secondary={
+					<Value
+						value={stakedBalance.toFixed(3)}
+						numberSuffix=" YAXIS"
 					/>
-				</Tooltip>
-				<Value value={totalApy.toFixed(2)} numberSuffix="%" />
-			</DetailOverviewCardRow>
+				}
+			/>
+			<CardRow
+				main={
+					<Tooltip
+						title={
+							<>
+								<Row>YAXIS APY:</Row>
+								<Row>{yaxAPY?.toFixed(2)}%</Row>
+								<Row>CRV APY (20%):</Row>
+								<Row>{metavaultAPY?.toFixed(2)}%</Row>
+							</>
+						}
+					>
+						<Text type="secondary">Total APY </Text>
+						<img
+							style={{ position: 'relative', top: -1 }}
+							src={info}
+							height="15"
+							alt="YAXIS Supply Rewards"
+						/>
+					</Tooltip>
+				}
+				secondary={
+					<Value value={totalApy.toFixed(2)} numberSuffix="%" />
+				}
+			/>
 		</DetailOverviewCard>
 	)
 }
