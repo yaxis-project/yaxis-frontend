d553fbb09d1fc3cd9ed6a4335ed5999c435beb4b
yakattack139 <yakattack139@protonmail.com>
2021-04-14 01:10:26 -0500

    Refactor: Account info into own component

--- src/components/TopBar/components/AccountButton.tsx
+++ src/components/TopBar/components/AccountButton.tsx
@@ -2,23 +2,17 @@ import React, { useCallback, useMemo } from 'react'
 import styled from 'styled-components'
 import useWeb3Provider from '../../../hooks/useWeb3Provider'
 import useModal from '../../../hooks/useModal'
-import { Button, Menu, Dropdown, Row, Col, Divider, Typography } from 'antd'
-import {
-	CaretDownOutlined,
-	CopyOutlined,
-	BlockOutlined,
-	CheckCircleTwoTone,
-} from '@ant-design/icons'
+import { Button, Menu, Dropdown, Row, Col } from 'antd'
+import { CaretDownOutlined } from '@ant-design/icons'
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
-import { etherscanUrl } from '../../../yaxis/utils'
 import WalletProviderModal from '../../WalletProviderModal'
+import AccountInfo from '../components/AccountInfo'
 import {
 	network,
 	NETWORK_NAMES,
 	FRIENDLY_NETWORK_NAMES,
 } from '../../../connectors'
 import { useWeb3React } from '@web3-react/core'
-const { Text } = Typography
 
 interface AccountButtonProps {}
 
@@ -63,6 +57,7 @@ const AccountButton: React.FC<AccountButtonProps> = (props) => {
 									account={account}
 									networkName={networkName}
 									friendlyNetworkName={friendlyNetworkName}
+									mobile={false}
 								/>
 								<Menu.Item>
 									<a
@@ -98,96 +93,9 @@ const AccountButton: React.FC<AccountButtonProps> = (props) => {
 	)
 }
 
-const AccountInfo = ({ account, networkName, friendlyNetworkName }) => {
-	return (
-		<>
-			<Menu.ItemGroup
-				title={
-					<Col
-						style={{
-							marginBottom: '5px',
-							paddingLeft: '10px',
-						}}
-					>
-						<StyledRow
-							style={{
-								margin: '5px 10px 12px 10px',
-							}}
-						>
-							<AccountText>Account:</AccountText>
-							<AccountIdText>
-								{account.slice(0, 4)} ... {account.slice(-2)}
-							</AccountIdText>
-						</StyledRow>
-						<StyledRow>
-							<Text
-								style={{
-									marginLeft: '-2px',
-								}}
-								copyable={{
-									icon: [
-										<Row>
-											<CopyOutlined key="copy-icon" />
-											<StyledText>
-												Copy Address
-											</StyledText>
-										</Row>,
-										<Row>
-											<CheckCircleTwoTone
-												key="copied-icon"
-												twoToneColor="#52c41a"
-											/>
-											<StyledText>
-												Address Copied!
-											</StyledText>
-										</Row>,
-									],
-									text: `${account}`,
-									tooltips: false,
-								}}
-							></Text>
-						</StyledRow>
-						<StyledRow>
-							<BlockOutlined />
-							<StyledText>
-								<a
-									href={etherscanUrl(
-										`/address/${account}`,
-										networkName,
-									)}
-									rel="noopener noreferrer"
-									target="_blank"
-								>
-									View on Etherscan
-								</a>
-							</StyledText>
-						</StyledRow>
-						<StyledRow>
-							<CheckCircleTwoTone twoToneColor="#52c41a" />{' '}
-							<StyledText>{friendlyNetworkName}</StyledText>
-						</StyledRow>
-						<Divider
-							orientation="left"
-							style={{
-								margin: '10px 0px 5px 0px',
-							}}
-						/>
-					</Col>
-				}
-			/>
-		</>
-	)
-}
-
 const StyledMenu = styled(Menu)`
 	margin: 8px -2px 0 0;
 	border: 2px solid lightgrey;
-`
-
-const StyledRow = styled(Row)`
-	display: flex;
-	align-items: center;
-	margin-bottom: 5px;
 `
 
 const StyledButton = styled(Button)`
@@ -213,28 +121,6 @@ const StyledAccountButton = styled(Row)`
 	@media only screen and (max-width: 1100px) {
 		font-size: 0.8rem;
 	}
-`
-
-const AccountText = styled.div`
-	font-weight: bold;
-	margin-left: -8px;
-`
-
-const AccountIdText = styled.div`
-	font-weight: bold;
-	font-size: 1em;
-	color: #016eac;
-	border: 1.5px solid #016eac;
-	border-radius: 20px;
-	padding: 2px 10px;
-	margin-left: 10px;
-`
-
-const StyledText = styled.div`
-	text-align: center;
-	font-size: 0.9em;
-	margin-left: 10px;
-	color: rgba(0, 0, 0, 0.45);
 `
 
 export default AccountButton
